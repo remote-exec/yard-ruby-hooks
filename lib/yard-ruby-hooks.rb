bed1a72f82633faa7fc1990c339855351c06c930
Michal Papis <mpapis@gmail.com>
2014-10-12 23:39:07 +0200

    use double quotes everywhere

--- lib/yard-ruby-hooks.rb
+++ lib/yard-ruby-hooks.rb
@@ -4,4 +4,4 @@ Copyright 2014 Michal Papis <mpapis@gmail.com>
 See the file LICENSE for copying permission.
 =end
 
-require File.join(File.dirname(__FILE__), 'yard-ruby-hooks', 'define_hook_handler')
+require File.join(File.dirname(__FILE__), "yard-ruby-hooks", "define_hook_handler")
